287f4831280673e4f5bf6cf9849268057f477e4d
efgeorge <fgeorge.enache@gmail.com>
2023-07-11 14:09:45 +0300

    changed font, centered the letters

--- src/components/Pad.js
+++ src/components/Pad.js
@@ -2,7 +2,7 @@ import './../styles/Pad.scss'
 
 const Pad = () => {
     const pads = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C'];
-    const padsOutput = pads.map(pad => <div id="pad">{pad}</div>);
+    const padsOutput = pads.map(pad => <button id="pad">{pad}</button>);
 
     return (
         <div id="drum-pad">
