36cde04f6f038fc00e7d88fd2d698f7bc2f4dd72
efgeorge <fgeorge.enache@gmail.com>
2023-07-14 14:17:34 +0300

    keys trigger the sound too

--- src/App.js
+++ src/App.js
@@ -1,48 +1,67 @@
 import './App.scss';
 import Pad from './components/Pad';
 import Controls from './components/Controls';
+import { useRef, useEffect } from 'react';
 
 function App() {
-  const pads = [
-    {'key': 'Q',
-     'source': 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'        
-    }, 
-    {'key': 'W',
-    'source': 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'        
-    }, 
-    {'key': 'E',
-     'source': 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'        
-    }, 
-    {'key': 'A',
-     'source': 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'        
-    }, 
-    {'key': 'S',
-     'source': 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'        
-    }, 
-    {'key': 'D',
-     'source': 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'        
-    }, 
-    {'key': 'Z',
-     'source': 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'        
-    }, 
-    {'key': 'X',
-     'source': 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'        
-    }, 
-    {'key': 'C',
-    'source': 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'        
-    }
-  ];
+  // const pads = [
+  //   {'key': 'Q',
+  //    'source': 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'        
+  //   }, 
+  //   {'key': 'W',
+  //   'source': 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'        
+  //   }, 
+  //   {'key': 'E',
+  //    'source': 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'        
+  //   }, 
+  //   {'key': 'A',
+  //    'source': 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'        
+  //   }, 
+  //   {'key': 'S',
+  //    'source': 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'        
+  //   }, 
+  //   {'key': 'D',
+  //    'source': 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'        
+  //   }, 
+  //   {'key': 'Z',
+  //    'source': 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'        
+  //   }, 
+  //   {'key': 'X',
+  //    'source': 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'        
+  //   }, 
+  //   {'key': 'C',
+  //   'source': 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'        
+  //   }
+  // ];
+  const pads = {
+    "Q": "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    "W": "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+    "E": "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+    "A": "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+    "S": "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+    "D": "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+    "Z": "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+    "X": "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+    "C": "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+  }
 
-  const handleKeyPress = (e, key, source) => {
-    if (e.key === key) {
-        new Audio(source).play();
-    }
-}
+  const handleKeyPress = (e) => {
+      new Audio(pads[e.key.toUpperCase()])?.play();
+      console.log(pads[e.key.toUpperCase()], "miau");
+  }
+
+  const ref = useRef(null);
+
+  useEffect(() => {
+    ref.current.focus();
+  }, [])
 
   return (
-    <div id="drum-machine">
-      <Pad pads={pads}/>
-      <Controls />
+    <div id="app-wrapper" onKeyDown={handleKeyPress} tabIndex={0} ref={ref}>
+      <div id="drum-machine">
+        <Pad pads={pads}/>
+        <Controls />
+      </div>
     </div>
   );
 }
--- src/components/Pad.js
+++ src/components/Pad.js
@@ -2,31 +2,11 @@ import './../styles/Pad.scss';
 import { useEffect, useRef } from 'react';
 
 const Pad = ({ pads }) => {
-    
-
-    // const handleKeyPress = (e) => {
-    //     if (e.key === "Q") {
-    //         new Audio('https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3').play();
-    //     }
-
-    // }
-    
-    // const handleKeyPress = (e, key,) => {
-    //     console.log(e.key);
-    // }
-
-    // const ref = useRef(null);
-    // useEffect(() => {
-    //     ref.current.focus();
-    // }, []);
-
-    const padsOutput = pads.map(pad => 
-        // ref={ref} onKeyDown={(e) => handleKeyPress(e, pad.key, pad.source)} tabIndex={0}
-        <div id="pad" onClick={() => new Audio(pad.source).play()} >
-            {pad.key}
+    const padsOutput = Object.entries(pads).map(([key, value]) => 
+        <div id="pad" onClick={() => new Audio(value).play()} >
+            {key}
         </div>
     );
-    
 
     return (
         <div id="drum-pad">
