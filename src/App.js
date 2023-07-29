989b51b2f6302072c3ad43d3a0e272562810e573
efgeorge <fgeorge.enache@gmail.com>
2023-07-29 23:47:56 +0300

    cleaned the code

--- src/App.js
+++ src/App.js
@@ -4,35 +4,6 @@ import Controls from './components/Controls';
 import { useRef, useEffect } from 'react';
 
 function App() {
-  // const pads = [
-  //   {'key': 'Q',
-  //    'source': 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'        
-  //   }, 
-  //   {'key': 'W',
-  //   'source': 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'        
-  //   }, 
-  //   {'key': 'E',
-  //    'source': 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'        
-  //   }, 
-  //   {'key': 'A',
-  //    'source': 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'        
-  //   }, 
-  //   {'key': 'S',
-  //    'source': 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'        
-  //   }, 
-  //   {'key': 'D',
-  //    'source': 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'        
-  //   }, 
-  //   {'key': 'Z',
-  //    'source': 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'        
-  //   }, 
-  //   {'key': 'X',
-  //    'source': 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'        
-  //   }, 
-  //   {'key': 'C',
-  //   'source': 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'        
-  //   }
-  // ];
   const pads = {
     "Q": "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
     "W": "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
